868e87c846cacefd30b1735b323b64c79a463ea6
EMIN YUCE <eminyuce@gmail.com>
2024-09-20 18:32:50 -0400

    remove unnecessary files

--- start.groovy
+++
@@ -1,5 +0,0 @@
-@Grab("com.acqu.co:spring-boot-starter-actuator:0.5.0.BUILD-SNAPSHOT")
-@Grab("com.acqu.co:spring-boot-starter-web:0.5.0.BUILD-SNAPSHOT")
-@ComponentScan("com.acqu.co.sample.service")
-class Start {
-}
