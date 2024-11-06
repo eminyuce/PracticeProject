913c34f55f49f846fb0c4ad84431799787e4bfb5
eminyuce <eminyuce@gmail.com>
2024-11-05 21:31:11 -0500

    code exercise v2

--- src/main/java/com/acqu/co/excel/converter/actuator/config/SwaggerConfig.java
+++ src/main/java/com/acqu/co/excel/converter/actuator/config/SwaggerConfig.java
@@ -5,20 +5,32 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI springShopOpenAPI() {
-        return new OpenAPI().components(new Components())
-                .info(new Info().title("acqu API").version("2.1.33").description("FIIS Service application")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .externalDocs(new ExternalDocumentation().description("SpringShop Wiki Documentation")
+        return new OpenAPI()
+                .info(new Info()
+                        .title("acqu API")
+                        .version("2.1.33")
+                        .description("Code Exercise Service application")
+                        .license(new io.swagger.v3.oas.models.info.License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringShop Wiki Documentation")
                         .url("https://springshop.wiki.github.org/docs"))
-                .components(new Components().addSecuritySchemes("bearer-key",
-                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearer-key", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .security(Arrays.asList(new SecurityRequirement().addList("bearer-key")));
     }
 }
--- src/main/java/com/acqu/co/excel/converter/actuator/controller/AcquUserEntityController.java
+++ src/main/java/com/acqu/co/excel/converter/actuator/controller/AcquUserEntityController.java
@@ -3,6 +3,10 @@ package com.acqu.co.excel.converter.actuator.controller;
 import com.acqu.co.excel.converter.actuator.model.AcquUserEntity;
 import com.acqu.co.excel.converter.actuator.service.AcquUserEntityService;
 import com.acqu.co.excel.converter.actuator.util.DateUtil;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.domain.Page;
@@ -11,6 +15,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -54,7 +59,16 @@ public class AcquUserEntityController {
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<List<AcquUserEntity>> uploadAcquUserEntityFromExcel(@RequestParam("file") MultipartFile file) {
+    @Operation(
+            summary = "Upload an Excel file with AcquUserEntity data",
+            description = "Upload a file containing user data in Excel format.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE)
+            )
+    )
+    public ResponseEntity<List<AcquUserEntity>> uploadAcquUserEntityFromExcel(
+            @Parameter(description = "Excel file to upload", required = true)
+            @RequestParam("file") MultipartFile file) {
         try {
             List<AcquUserEntity> users = acquUserEntityService.uploadAcquUserEntityFromExcel(file);
             return new ResponseEntity<>(users, HttpStatus.CREATED);
